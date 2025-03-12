9724b6f44d8c8153ea754864fe4fe1c7c58303b8
ASHIQANWARK <ashiqanwar777@gmail.com>
2025-03-12 15:38:40 +0530

    custom commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { FaArrowUp } from "react-icons/fa";
+import React from "react";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import Team from "../components/Team";
-import ChatBot from "./Chatbot";
-import WhyFranchisify from "./WhyChooseus";
-import Customers from "./Ourcompanies";
+import ChatBot from "../pages/Chatbot";
+import WhyFranchisify from "../pages/WhyChooseus"; // Ensure correct casing
+import Customers from "../pages/Ourcompanies"; // Ensure correct casing
 
 const Home = () => {
   return (
-    <div className="relative">
+    <div>
       <Hero />
       <WhyFranchisify />
       <Services />
       <Customers />
       <Team />
       <ChatBot />
-
     </div>
   );
 };
 
 export default Home;
-
